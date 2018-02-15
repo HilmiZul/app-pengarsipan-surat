b5f9f28557564e5db86db249f2557e5fe1b0a268
Zul Hilmi <netspytux@gmail.com>
2018-02-15 14:18:43 +0700

    menambahkan proses hapus data

--- konten/surat-masuk.php
+++ konten/surat-masuk.php
@@ -1,12 +1,24 @@
+<?php
+if ($_GET['hapus'] == 'ya') {
+  $no_agenda = $_GET['no_agenda'];
+
+  mysql_query("delete from tb_surat_masuk
+              where no_agenda=$no_agenda");
+
+  $pesan = "<div class='alert alert-success'>Data berhasil dihapus.</div>";
+}
+ ?>
+
 <section class="">
   <div class="container">
     <div class="row">
-      <div class="col text-center">
+      <div class="col">
         <div class="card">
           <div class="card-header text-center">
             <h4>Catatan Surat Masuk <a href="?menu=tambah-surat-masuk" class="btn btn-primary">Tambah</a></h4>
           </div>
           <div class="card-body">
+            <?php print $pesan?>
             <div class="table-responsive">
               <table class="table table-striped table-hover table-bordered">
                 <thead>
@@ -32,8 +44,23 @@
                     <td><?php print $r['tgl_terima']?></td>
                     <td><?php print $r['perihal']?></td>
                     <td><?php print $r['pengirim']?></td>
-                    <td><a href="#" class="btn btn-danger">Hapus</a></td>
+                    <td>
+                      <a href="?menu=ubah-surat-masuk&no_agenda=<?php print $r['no_agenda']?>" class="btn btn-warning">Ubah</a>
+                      <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#no_agenda_<?php print $r['no_agenda']?>">Hapus</a>
+                    </td>
                   </tr>
+                  <div class="modal fade" id="no_agenda_<?php print $r['no_agenda']?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-body">
+                          Apakah Anda yakin akan menghapus catatan surat dari: <strong><?php print $r['pengirim']?></strong>?
+                        </div>
+                        <div class="modal-footer">
+                          <a href="?menu=surat-masuk&hapus=ya&no_agenda=<?php print $r['no_agenda']?>" class="btn btn-danger">Ya, Hapus</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 <?php } // tutup while ?>
                 </tbody>
               </table>
