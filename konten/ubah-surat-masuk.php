163108ff49b63957e409191bc63cf0d2a030d5a3
Zul Hilmi <netspytux@gmail.com>
2018-02-16 19:06:58 +0700

    validasi form input

--- konten/ubah-surat-masuk.php
+++ konten/ubah-surat-masuk.php
@@ -7,7 +7,6 @@ if (isset($_POST['simpan'])) {
   $pengirim = $_POST['pengirim'];
   $perihal = $_POST['perihal'];
   $no_agenda = $_POST['no_agenda'];
-
   mysql_query("update tb_surat_masuk set
               no_surat='$no_surat', jenis_surat='$jenis_surat',
               tgl_kirim='$tgl_kirim', tgl_terima='$tgl_terima',
@@ -15,9 +14,6 @@ if (isset($_POST['simpan'])) {
               where no_agenda=$no_agenda");
   $pesan = "<div class='alert alert-success'>Data berhasil diubah.</div>";
 }
-
-
-
 $no_agenda = $_GET['no_agenda'];
 $q = mysql_query("select * from tb_surat_masuk
                   where no_agenda=$no_agenda");
@@ -37,27 +33,27 @@ $r = mysql_fetch_array($q);
             <form action="" method="post">
               <div class="form-group">
                 <div class="label">Jenis Surat</div>
-                <input type="text" class="form-control" name="jenis_surat" value="<?php print $r['jenis_surat']?>" placeholder="Jenis Surat">
+                <input type="text" class="form-control" name="jenis_surat" value="<?php print $r['jenis_surat']?>" placeholder="Jenis Surat" autofocus required>
               </div>
               <div class="form-group">
                 <div class="label">Nomor Surat</div>
-                <input type="text" class="form-control" name="no_surat" value="<?php print $r['no_surat']?>" placeholder="Nomor Surat. ex: SMK/17/18/VI">
+                <input type="text" class="form-control" name="no_surat" value="<?php print $r['no_surat']?>" placeholder="Nomor Surat. ex: SMK/17/18/VI" required>
               </div>
               <div class="form-group">
                 <div class="label">Tanggal Kirim</div>
-                <input type="date" class="form-control" name="tgl_kirim" value="<?php print $r['tgl_kirim']?>">
+                <input type="date" class="form-control" name="tgl_kirim" value="<?php print $r['tgl_kirim']?>" required>
               </div>
               <div class="form-group">
                 <div class="label">Tanggal Terima</div>
-                <input type="date" class="form-control" name="tgl_terima" value="<?php print $r['tgl_terima']?>">
+                <input type="date" class="form-control" name="tgl_terima" value="<?php print $r['tgl_terima']?>" required>
               </div>
               <div class="form-group">
                 <div class="label">Pengirim</div>
-                <input type="text" class="form-control" name="pengirim" value="<?php print $r['pengirim']?>" placeholder="Pengirim">
+                <input type="text" class="form-control" name="pengirim" value="<?php print $r['pengirim']?>" placeholder="Pengirim" required>
               </div>
               <div class="form-group">
                 <div class="label">Perihal</div>
-                <input type="text" class="form-control" name="perihal" value="<?php print $r['perihal']?>" placeholder="Perihal">
+                <input type="text" class="form-control" name="perihal" value="<?php print $r['perihal']?>" placeholder="Perihal" required>
               </div>
               <input type="hidden" name="no_agenda" value="<?php print $r['no_agenda']?>">
 
