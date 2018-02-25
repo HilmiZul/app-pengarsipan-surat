ead65a3bb10ac822dd52346df0a5b248e22957f4
Zul Hilmi <netspytux@gmail.com>
2018-02-25 13:19:51 +0700

    initial commit

---
+++ konten/laporan-surat-keluar.php
@@ -0,0 +1,30 @@
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-header text-center">
+            <h4>Laporan Surat Keluar</h4>
+          </div>
+          <div class="card-body">
+            <form action="konten/rincian-laporan-surat-keluar.php" method="post">
+              <div class="form-group">
+                <div class="label">
+                  Filter berdasarkan tanggal kirim
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="">Tanggal Awal:</label>
+                <input type="date" name="tgl_awal"> &nbsp;
+
+                <label for="">Tanggal Akhir:</label>
+                <input type="date" name="tgl_akhir"> &nbsp;
+              </div>
+              <button type="submit" name="kirim" class="btn btn-primary">Kirim</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
